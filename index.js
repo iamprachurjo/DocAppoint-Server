c8318e40a4580db6f1243cd1068d3b7dfda04687
Prachurjo <prachurjo.barmon.bd@gmail.com>
2026-08-16 20:56:54 +0600

    fetching bookings api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = process.env.MONGODB_URI;
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 
 const PORT = process.env.PORT;
 
@@ -24,27 +24,39 @@ async function run() {
 
     // DB creation
     const db = client.db("Prescripto");
+    // Collection 
     const doctorsCollection = db.collection("doctors");
+    const bookingCollection = db.collection("bookings")
+
 
     // create function added
-    app.post('/doctors', async(req, res) => {
+    app.post("/doctors", async (req, res) => {
       const doctorData = req.body;
-      const result = await doctorsCollection.insertOne(doctorData)
+      const result = await doctorsCollection.insertOne(doctorData);
+      res.json(result);
+    });
+
+    // 
+    app.post("/bookings", async(req, res) => {
+      const bookingData = req.body;
+      const result = await bookingCollection.insertOne(bookingData)
       res.json(result)
     })
 
     // Get/read doctors from api
-    app.get('/doctors', async(req, res) =>{
-      const result = await doctorsCollection.find().toArray()
-      res.json(result)
-    })
+    app.get("/doctors", async (req, res) => {
+      const result = await doctorsCollection.find().toArray();
+      res.json(result);
+    });
 
     // Get Single doctor data
-    app.get('/appointments/:id', async(req, res) => {
-      const {id} = req.params;
-      const result = await doctorsCollection.findOne({_id: new ObjectId(id)})
-      res.json(result)
-    })
+    app.get("/appointments/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await doctorsCollection.findOne({ _id: new ObjectId(id) });
+      res.json(result);
+    });
+
+    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
